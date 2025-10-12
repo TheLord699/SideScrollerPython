bdc11f9b4cf97c6aaa9fd88487e15a6aaaf64914
TheLord699 <146233216+TheLord699@users.noreply.github.com>
2025-10-12 09:19:21 +0100

    removing deprecated from tracking

--- assets/source/deprecated/delta_time.py
+++
@@ -1,7 +0,0 @@
-import pygame as pg
-
-def update_delta_time(self):
-    self.current_time = pg.time.get_ticks()
-    self.delta_time = (self.current_time - self.last_time) / 1000.0
-    self.last_time = self.current_time
-    
--- assets/source/deprecated/sheet_button.py
+++
@@ -1,9 +0,0 @@
-""""
-self.game.ui.create_ui(
-    x=100, y=100, width=32, height=32, 
-    sprite_sheet_path="assets/sprites/gui/items/Sheet.png", 
-    sprite_width=16, sprite_height=16, 
-    image_id="item_0_0",  # Image at index 5 in the sprite sheet
-    element_id="sprite_button", 
-)
-"""
